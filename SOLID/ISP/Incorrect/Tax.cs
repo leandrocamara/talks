5ec2f1c722d22ab0ab65295c9a92ebd2cd12844d
Leandro Câmara <leandrocamara_eng@outlook.com>
2022-11-09 21:48:44 -0300

    Exemplos dos princípios SOLID

---
+++ SOLID/ISP/Incorrect/Tax.cs
@@ -0,0 +1,26 @@
+﻿using SOLID.ISP.Common;
+
+namespace SOLID.ISP.Incorrect;
+
+public interface ITax
+{
+    Invoice GenerateInvoce();
+    double GenerateTax(double value);
+}
+
+public class Iss : ITax
+{
+    public Invoice GenerateInvoce() => new Invoice();
+
+    public double GenerateTax(double value) => value * 0.1;
+}
+
+public class Ixmx : ITax
+{
+    public Invoice GenerateInvoce() => throw new NotSupportedException();
+
+    public double GenerateTax(double value) => value * 0.2;
+}
+
+// A interface ITax não é coesa
+// A interface ITax está obrigado a classe Ixmx implementar algo que não é preciso
